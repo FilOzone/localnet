954f79d749895a73441e59909c09b8e4cefa4a23
William Morriss <wjmelements@gmail.com>
2025-07-30 17:03:58 -0500

    move seed to setup

--- reset.sh
+++ reset.sh
@@ -3,23 +3,18 @@ export LOTUS_MINER_PATH=~/.lotus-miner-local-net
 export LOTUS_SKIP_GENESIS_CHECK=_yes_
 export CGO_CFLAGS_ALLOW="-D__BLST_PORTABLE__"
 export CGO_CFLAGS="-D__BLST_PORTABLE__"
+export LOTUS_CHAININDEXER_ENABLEINDEXER=1
 export LOTUS_FEVM_ENABLEETHRPC=1
 
 
 
 
-rm -rf ~/.genesis-sectors
-rm -rf $LOTUS_MINER_PATH
+rm -rf $LOTUS_PATH $LOTUS_MINER_PATH
 
 cd lotus-local-net
-./lotus fetch-params 2048
-
-./lotus-seed pre-seal --sector-size 2KiB --num-sectors 2
-./lotus-seed genesis new localnet.json
-./lotus-seed genesis add-miner localnet.json ~/.genesis-sectors/pre-seal-t01000.json
 
 echo starting daemon
-./lotus daemon --lotus-make-genesis=devgen.car --genesis-template=localnet.json --bootstrap=false &> daemon.log &
+./lotus daemon --lotus-make-genesis=devgen.car --genesis-template=localnet.json --bootstrap=false &
 trap "kill -2 $!" EXIT
 until [ -e $LOTUS_PATH/api ]; do
     sleep 0.01
--- setup.sh
+++ setup.sh
@@ -15,3 +15,11 @@ git clone -b $TAG --depth 1 https://github.com/filecoin-project/lotus lotus-loca
 cd lotus-local-net
 
 make 2k
+
+#rm -rf ~/.genesis-sectors
+
+./lotus fetch-params 2048
+
+./lotus-seed pre-seal --sector-size 2KiB --num-sectors 2
+./lotus-seed genesis new localnet.json
+./lotus-seed genesis add-miner localnet.json ~/.genesis-sectors/pre-seal-t01000.json
