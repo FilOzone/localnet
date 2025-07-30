0d1a0c5fa3741f5c1ab093d5f2a9f184fe0233b9
William Morriss <wjmelements@gmail.com>
2025-07-30 17:12:15 -0500

    reset.sh: cat daemon.log on daemon failure

--- reset.sh
+++ reset.sh
@@ -13,14 +13,22 @@ rm -rf $LOTUS_PATH $LOTUS_MINER_PATH
 
 cd lotus-local-net
 
-echo starting daemon
-./lotus daemon --lotus-make-genesis=devgen.car --genesis-template=localnet.json --bootstrap=false &
-trap "kill -2 $!" EXIT
+echo Awaiting Lotus Daemon API...
+./lotus daemon --lotus-make-genesis=devgen.car --genesis-template=localnet.json --bootstrap=false &> daemon.log &
+DAEMON_PID=$!
+trap "kill -2 $DAEMON_PID 2>/dev/null" EXIT
 until [ -e $LOTUS_PATH/api ]; do
-    sleep 0.01
+    if kill -0 $DAEMON_PID 2>&1
+    then
+        sleep 0.01
+    else
+        # died before starting API
+        cat daemon.log
+        exit 1
+    fi
 done
 
-echo importing wallet
+echo Importing genesis wallet
 ./lotus wallet import --as-default ~/.genesis-sectors/pre-seal-t01000.key 
-echo init miner
+echo Init genesis miner
 ./lotus-miner init --genesis-miner --actor=t01000 --sector-size=2KiB --pre-sealed-sectors=~/.genesis-sectors --pre-sealed-metadata=~/.genesis-sectors/pre-seal-t01000.json --nosync 
