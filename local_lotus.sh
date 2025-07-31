1dff0c152981d7ddc5e8ddbaa48cbac34e8549b6
William Morriss <wjmelements@gmail.com>
2025-07-31 16:59:24 -0500

    measure durations

--- local_lotus.sh
+++ local_lotus.sh
@@ -14,21 +14,26 @@ while [ -e $LOTUS_PATH/repo.lock ]; do
     sleep 5
 done
 
+API_START=$(date +%s.%N)
 ./lotus daemon --bootstrap=false &> daemon.log &
 DAEMON_PID=$!
 EXIT_TRAP="kill -2 $DAEMON_PID 2>&1" 
 trap "$EXIT_TRAP" EXIT
 
-echo Awaiting Lotus Daemon API...
+echo -n "Awaiting Lotus Daemon API...    "
 until [ -e $LOTUS_PATH/api ]; do
     if kill -0 $DAEMON_PID
     then
         sleep 0.01
     else
+        echo
         cat daemon.log
         exit 1
     fi
 done
+
+API_END=$(date +%s.%N)
+echo `echo $API_END - $API_START | bc`
 
 
 f4=$(./lotus wallet new delegated)
@@ -42,10 +47,13 @@ done
 EXIT_TRAP+="; kill -2 $!"
 trap "$EXIT_TRAP" EXIT
 
+MINING_START=$(date +%s.%N)
 FUNDING_MSG=$(./lotus send $f4 1 | tail -n 1)
 
-echo Awaiting funding...
+echo -n "Awaiting funding...    "
 FUNDING_RECEIPT=$(./lotus state wait-msg $FUNDING_MSG)
+MINING_END=$(date +%s.%N)
+echo `echo $MINING_END - $MINING_START | bc`
 
 echo -e "$FUNDING_RECEIPT"
 
